6d3b17e2324b0052ffb00f6aa0500b89ac1aa26d
Jeffrey Brown <Jeffrey Brown>
2017-03-30 00:21:48 -0400

    before-buttons

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
-var gifAjxObjs;
-
 function animalGifDivConstructor(imgSrc, rating, id) {
+  //construct a div for image object with rating and image source
+  //parent div each object with class and id
   var animalGifDiv = $("<div>");
   $(animalGifDiv).attr("class", "animalGifDiv");
   $(animalGifDiv).attr("id", "aGD" + id);
+  // p tag for rating display
   var p = $("<p>").text("Rating: " + rating);
+  //image source duh
   var animalImage = $("<img>");
-  // "http://media0.giphy.com/media/l2SpLKjTKEBhYqbbq/200.gif"
   animalImage.attr("src", imgSrc);
+  //append the children to the parent
   animalGifDiv.append(p);
   animalGifDiv.append(animalImage);
-  $("#animalButtons").prepend(animalGifDiv);
+  //prepend the collective object into the buttons div (this will later be the animals div)
+  $("#animals").prepend(animalGifDiv);
 }
 
 function loadAjxObjs(query) {
+  // AJAX query to pull down the GIF objects by animal name (query) and put them into the HTML
   $.ajax({
   	url: "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q="+query+"&limit=9&rating=r",
   	method: "GET"
   	}).done(function(response) { 
       gifs = response.data;
       console.log("gifs.length=", gifs.length);
-      for (i=0; i<gifs.length; i++) {
-	      animalGifDivConstructor(gifs[i].images.fixed_height.url, gifs[i].rating, i);
-	  }
+      // self explanatory if nothing found
+      if (gifs.length === 0) {
+        $("#animals").empty();
+        $("#animals").html("Oops, nothing found.");       
+      } else {
+        //run the div constructor for each image object
+        for (i=0; i<gifs.length; i++) {animalGifDivConstructor(gifs[i].images.fixed_height.url, gifs[i].rating, i)}
+      }
   });
 }
 
 $(document).on("click", "#b0", function() {
-  $("#animalButtons").empty();
+  // grab the input
 	var animalQuery = $("#i0").val();
-	if (animalQuery !== "") {loadAjxObjs(animalQuery)};
+  //only do something if there's input present
+	if (animalQuery !== "") {
+    //clear out whatever was there before
+    $("#animals").empty();
+    //do my thang
+    loadAjxObjs(animalQuery)
+  }
 });
